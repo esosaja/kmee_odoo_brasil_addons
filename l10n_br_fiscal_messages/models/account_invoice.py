c25b13de3826e4598e3055b8596ece09ffeaa990
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2016-03-09 18:29:25 -0300

    [FIX] Agora o modulo aceita caso o usuario nao tenha criado nenhuma mensagem fiscal e arrumado funcao nfe_export() chamando o super para continuar o processo

--- l10n_br_fiscal_messages/models/account_invoice.py
+++ l10n_br_fiscal_messages/models/account_invoice.py
@@ -19,38 +19,16 @@
 #
 ###############################################################################
 
-import os
-import logging
-import datetime
-
-from openerp.tools.translate import _
 from openerp import models, fields, api
-from openerp.exceptions import RedirectWarning
-
-from openerp.addons.nfe.sped.nfe.nfe_factory import NfeFactory
-from openerp.addons.nfe.sped.nfe.validator.xml import XMLValidator
-from openerp.addons.nfe.sped.nfe.processing.xml import send, cancel
-from openerp.addons.nfe.sped.nfe.processing.xml import monta_caminho_nfe
-from openerp.addons.nfe.sped.nfe.processing.xml import check_key_nfe
-from openerp.addons.nfe.sped.nfe.validator.config_check import \
-    validate_nfe_configuration, validate_invoice_cancel
-
-_logger = logging.getLogger(__name__)
 
 
-class AccountInvoice(models.Model):
-    """account_invoice overwritten methods"""
+class AccountInvoiceFiscalMessage(models.Model):
     _inherit = 'account.invoice'
 
     @api.multi
     def nfe_export(self):
 
         for inv in self:
-
-            validate_nfe_configuration(inv.company_id)
-
-            nfe_obj = self._get_nfe_factory(inv.nfe_version)
-
             messages_invoice = []
 
             messages_invoice.append(
@@ -69,7 +47,7 @@ class AccountInvoice(models.Model):
                             inv.fiscal_category_id.message_id.message_invoice
                         )
 
-            if messages_invoice:
+            if messages_invoice[0]:
                 messages_list = []
 
                 for message in messages_invoice:
@@ -81,49 +59,4 @@ class AccountInvoice(models.Model):
                 else:
                     inv.comment = ' - '.join(messages_list)
 
-            # nfe_obj = NFe310()
-            nfes = nfe_obj.get_xml(self.env.cr, self.env.uid, self.ids,
-                                   int(inv.company_id.nfe_environment),
-                                   self.env.context)
-
-            for nfe in nfes:
-                # erro = nfe_obj.validation(nfe['nfe'])
-                erro = XMLValidator.validation(nfe['nfe'], nfe_obj)
-                nfe_key = nfe['key'][3:]
-                if erro:
-                    raise RedirectWarning(
-                        erro, _(u'Erro na validaço da NFe!'))
-
-                inv.write({'nfe_access_key': nfe_key})
-                save_dir = os.path.join(
-                    monta_caminho_nfe(
-                        inv.company_id,
-                        chave_nfe=nfe_key) +
-                    'tmp/')
-                nfe_file = nfe['nfe'].encode('utf8')
-
-                file_path = save_dir + nfe_key + '-nfe.xml'
-                try:
-                    if not os.path.exists(save_dir):
-                        os.makedirs(save_dir)
-                    f = open(file_path, 'w')
-                except IOError:
-                    raise RedirectWarning(
-                        _(u'Erro!'), _(u"""Não foi possível salvar o arquivo
-                            em disco, verifique as permissões de escrita
-                            e o caminho da pasta"""))
-                else:
-                    f.write(nfe_file)
-                    f.close()
-
-                    event_obj = self.env['l10n_br_account.document_event']
-                    event_obj.create({
-                        'type': '0',
-                        'company_id': inv.company_id.id,
-                        'origin': '[NF-E]' + inv.internal_number,
-                        'file_sent': file_path,
-                        'create_date': datetime.datetime.now(),
-                        'state': 'draft',
-                        'document_event_ids': inv.id
-                    })
-                    inv.write({'state': 'sefaz_export'})
+            super(AccountInvoiceFiscalMessage, self).nfe_export()
